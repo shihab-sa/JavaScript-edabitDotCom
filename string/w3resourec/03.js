6213473e97fe602cee2be36837bf56098f565515
shihab <game123ahmed@gmail.com>
2021-11-13 20:54:56 +0600

    new code solved

---
+++ string/w3resourec/03.js
@@ -0,0 +1,17 @@
+/* 3. Write a JavaScript function to split a string and convert it into an array of words. Go to the editor
+Test Data :
+console.log(string_to_array("Robin Singh"));
+["Robin", "Singh"]  */
+
+
+
+function StringtoArray(str){
+
+    let result = str.split(' ',2)   // split devited to string like two name convation 
+    return result
+
+
+
+}
+
+console.log(StringtoArray('Shihab ahmed'));
---
+++ string/w3resourec/04.js
@@ -0,0 +1,22 @@
+/*   
+4. Write a JavaScript function to extract a specified number of characters from a string. Go to the editor
+Test Data :
+console.log(truncate_string("Robin Singh",4));
+"Robi"
+
+
+
+*/
+
+
+
+// function truncateString(str,len){
+//    return str.slice(0,len)
+
+// }
+
+// let result = truncateString('shihab ahmed',4)
+// console.log(result);
+
+// let result1 = truncateString('Robin Singh',4)
+// console.log(result1);
