98f8842116b0bbd59956f1d54e6abf2b6db738fa
shihab <game123ahmed@gmail.com>
2021-11-21 18:57:25 +0600

    new solved

---
+++ Array/w3resourece/08.js
@@ -0,0 +1,4 @@
+// let arr= [22,200,33,44,100,200,400]
+
+// console.log(arr.sort((a,b)=>b-a));  // for descending sort 
+// console.log(arr.sort((a,b)=>a-b));  //  ascending sort
--- Array/w3resourece/Note.js
+++ Array/w3resourece/Note.js
@@ -1,3 +1,3 @@
-// pending 1,3,7
+// pending 1,3,7,10
 
  // have finished 2,5,7
