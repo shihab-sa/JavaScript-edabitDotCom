bcfc106787dff569b1672ce7cd5c21033527f091
shihab <game123ahmed@gmail.com>
2022-01-13 17:27:49 +0600

    array problem solved

--- Array/w3resourece/01.js
+++ Array/w3resourece/01.js
@@ -9,19 +9,67 @@ true
 */
 
 
-function isArray(arr){
+// function isArray(arr){
 
-    if(Object.prototype.toString.call(arr)=='[object Array]'){
-        return true
-    }
+//     if(Object.prototype.toString.call(arr)=='[object Array]'){
+//         return true
+//     }
 
-    else{
-        return false
-    }
-}
+//     else{
+//         return false
+//     }
+// }
 
-let result = isArray('w3resource')
-console.log(result);
+// let result = isArray('w3resource')
+// console.log(result);
 
-let result2 = isArray([1,2,3,4,5])
-console.log(result2);
+// let result2 = isArray([1,2,3,4,5])
+// console.log(result2);
+
+
+
+
+// function isArray(arr){
+//     if(Object.prototype.toString.call(arr) == "[object Array]"){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// let result1 = isArray([1, 2, 4, 0])
+// console.log(result1);
+
+// let result2 = isArray("shihab")
+// console.log(result2);
+
+
+// let result = [1,2,3]
+
+// let result1 = Array.isArray(result)
+// let result2 = Array.isArray({x:23})
+// console.log(result1);
+// console.log(result2);
+
+
+
+// function isArray(arr){
+//     if(Array.isArray(arr)){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// let x1 = isArray("shihab")
+// console.log(x1);
+
+// let x2 = isArray([2,34])
+// console.log(x2);
+
+
+// let arr = [1,2,3]
+
+// let findArray = arr => Array.isArray(arr)
+// console.log(findArray);
