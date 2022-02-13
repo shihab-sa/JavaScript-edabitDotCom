eec46c11b688a679dde84b1b1bd23648b016ac8b
shihab <game123ahmed@gmail.com>
2022-02-13 18:28:28 +0600

    new array methods

--- Array/10killerMethods/03.js
+++ Array/10killerMethods/03.js
@@ -27,34 +27,90 @@ let arr = [1,10,22,12,-1,-2,4,6,7,10]
 // console.log(arr);
 
 
-let myInfo = [
+// let myInfo = [
+//     {
+//         name:'shihab',
+//         id:3,
+//         age:23
+//     },
+//     {
+//         name:'shihab',
+//         id:2,
+//         age:24
+//     },
+//     {
+//         name:'shihab',
+//         id:4,
+//         age:15
+//     }
+// ]
+
+// myInfo.sort((a,b)=>{
+//     if(a.id>b.id){
+//         return 1 
+//     }
+//     else if(a.id<b.id){
+//         return -1
+//     }
+//     else{
+//         return 0
+//     }
+// })
+
+// console.log(myInfo);
+
+
+let myBio = [
     {
-        name:'shihab',
+        id:1,
+        age:23,
+        name:'shihab ahmed'
+    },
+    {
         id:3,
-        age:23
+        age:25,
+        name:'ahmed'
     },
     {
-        name:'shihab',
         id:2,
-        age:24
+        age:44,
+        name:'boy'
     },
     {
-        name:'shihab',
-        id:4,
-        age:15
+        id:-4,
+        age:12,
+        name:'rakib'
     }
+
 ]
 
-myInfo.sort((a,b)=>{
-    if(a.id>b.id){
-        return 1 
+
+// let result = myBio.sort((a,b)=>{
+//     if(a.id>b.id){
+//         return 1 
+//     }
+//     else if(a.id<b.id){
+//         return -1
+//     }
+//     else{
+//         return 0 
+//     }
+// })
+
+
+// console.log(result);
+
+let finalResult = myBio.sort((a,b)=>{
+    if(a.age>b.age){
+        return 1
     }
-    else if(a.id<b.id){
+    else if(a.age<b.age){
         return -1
     }
     else{
-        return 0
+        return 0 
     }
 })
 
-console.log(myInfo);
+console.log(finalResult);
+
