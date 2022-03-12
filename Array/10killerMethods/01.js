2e7620648e1f08b77ec18f7d99a84585a3c309bc
shihab <game123ahmed@gmail.com>
2022-03-12 18:51:07 +0600

    new array methods added

--- Array/10killerMethods/01.js
+++ Array/10killerMethods/01.js
@@ -59,21 +59,92 @@
 // implementing find and findIndex Methods 
 
 
-let arr = [1,2,3,4,5,6,8,9,10]
+// let arr = [1,2,3,4,5,6,8,9,10]
 
-function findArray(arr,cb){
+// function findArray(arr,cb){
+//     for(let i=0; i<arr.length; i++){
+//         if(cb(arr[i])){
+//             // return arr[i]  //  return an array value
+//             // return i // return an array index 
+//         }
+//     }
+// }
+
+
+// let result = findArray(arr,function(arr){
+
+//     return  arr == 10
+
+// })
+
+// console.log(result);
+
+
+
+
+/// Array Find Methods 
+
+
+// let arr = [1,2,3,4,5,8,20]
+// let result = arr.find((x)=>{
+    
+// })
+
+// console.log(result);
+
+
+
+
+let arr = [1,23,11,23,2,3,4,5,6,8]
+
+
+// let result = arr.find((x)=>{
+//     return x == 29 
+// })
+
+// console.log(result);
+
+
+// let res = arr.findIndex((x)=>{
+//     return x == 3 
+// })
+
+// console.log(res);
+
+
+
+// function findM(arr,cb){
+
+//     for(let i=0; i<arr.length; i++){
+//         if(cb(arr[i])){
+//             return arr[i]
+//         }
+//     }
+
+
+
+// }
+
+// let result = findM(arr,function(x){
+//     return x == 30
+// })
+
+// console.log(result);
+
+
+
+function findIndex(arr,cb){
     for(let i=0; i<arr.length; i++){
         if(cb(arr[i])){
-            // return arr[i]  //  return an array value
-            // return i // return an array index 
+            return i 
         }
     }
+
 }
 
 
-let result = findArray(arr,function(arr){
-
-    return  arr == 10
+let result = findIndex(arr,function(x){
+    return x == 2
 
 })
 
